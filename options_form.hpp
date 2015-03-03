d6fac451f505bd79a70e1528e15408dc4088adb6
Loïc Bourgois <loicbourgois.web@gmail.com>
2015-03-03 09:34:08 +0100

    Refactor

--- options_form.hpp
+++ options_form.hpp
@@ -9,10 +9,8 @@ class OptionsForm;
 class OptionsForm : public QWidget
 {
     Q_OBJECT
-
 public:
     explicit OptionsForm(QWidget *parent = 0);
     ~OptionsForm();
     Ui::OptionsForm * ui;
-
 };
