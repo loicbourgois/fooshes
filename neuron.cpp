d1a0c128208d9f43d969e549498240b787d160bb
Loïc Bourgois <loicbourgois.web@gmail.com>
2015-03-03 09:17:18 +0100

    Change way the final output is calculated : now based on sum of coefficients, instead of coefficient count.

--- neuron.cpp
+++ neuron.cpp
@@ -1,13 +1,11 @@
 #include "neuron.hpp"
 
-#include "QDebug"
+#include <QDebug>
 
 Neuron::Neuron(std::vector<float> coefficients, unsigned int inputCount)
 {
     if(coefficients.size() != inputCount)
-    {
-        qDebug() << "probleme dans neuron";
-    }
+        qDebug() << "Problem in neuron : inputCount != coefficientCount";
     this->coefficients = coefficients;
     outputValue = coefficients[0];
 }
@@ -26,11 +24,13 @@ void Neuron::compute(std::vector<float> inputValues)
         for(unsigned int i = 0 ; i < inputValues.size() ; i++)
         {
             if(inputValues[i]>1 || inputValues[i]<-1)
-                qDebug() << "Input value >  < 1 ";
+                qDebug() << "Input value >  < 1 : " << inputValues[i];
             outputValue += inputValues[i] * coefficients[i];
-            divisor += coefficients[i];
+            divisor += fabs(coefficients[i]);
         }
-        outputValue /= coefficients.size();
+        outputValue /= divisor;
+        if(outputValue>1 || outputValue<-1)
+            qDebug() << "output value >  < 1 : " << outputValue;
     }
     else
         qDebug() << "Coefficient count not matching inputs.";
